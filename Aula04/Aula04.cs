0ed5f75dc5c8bfd03b59e712efcef9a28878d2e3
Raphaela Mesquita <raphaelammn@gmail.com>
2022-10-27 12:47:53 -0300

    adding global var def

--- Aula04/Aula04.cs
+++ Aula04/Aula04.cs
@@ -1,8 +1,10 @@
     using System; 
-// Variáveis locais e globais
-// Sobre Escopo:
-// Onde a variável é visível, até onde ela pode ser acessada e onde ela foi declarada. 
-    class Aula04{
+
+// Variáveis Globais são aquelas declaradas no início de um algoritmo. Podem ser utilizadas no algoritmo principal e por todos os outros subalgoritmos. 
+// Variáveis Locais são aquelas declaradas no início de um subalgoritmo.
+// Escopo de variáveis: onde essa variável é visível no código.
+
+
 static int num=10;
         static void Main(){
             int num2 =0;
@@ -13,4 +15,4 @@ static int num=10;
             int num3=0; // Mesmo com o mesmo nome e valor, elas não são a mesma variável. 
                 Console.WriteLine(num);
         }
-    } // Escopo de variáveis: onde essa variável é visível no código.
+    
