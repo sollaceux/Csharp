db8b12fe4a57d3b136a8e0a2ed9f20027b3cbc4c
Raphaela Mesquita <raphaelammn@gmail.com>
2022-10-27 12:43:08 -0300

    setting global var def

--- Aula04/Aula04.cs
+++ Aula04/Aula04.cs
@@ -1,15 +1,16 @@
     using System; 
 // Variáveis locais e globais
 // Sobre Escopo:
+// Onde a variável é visível, até onde ela pode ser acessada e onde ela foi declarada. 
     class Aula04{
 static int num=10;
         static void Main(){
-        int num2 =0;
-        Console.WriteLine(num);
+            int num2 =0;
+            Console.WriteLine(num);
         } // Variável local já finalizada, logo, precisa declarar uma nova variável num.
 
-    void teste(){
-        int num3=0; // Mesmo com o mesmo nome e valor, elas não são a mesma variável. 
-        Console.WriteLine(num);
+        void teste(){
+            int num3=0; // Mesmo com o mesmo nome e valor, elas não são a mesma variável. 
+                Console.WriteLine(num);
         }
     } // Escopo de variáveis: onde essa variável é visível no código.
